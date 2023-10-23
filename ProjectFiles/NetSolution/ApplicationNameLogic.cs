47bd093730603efd491d308e5b4bc7f099733cd8
Weefoobah <asanders@ra.rockwell.com>
2023-10-23 11:01:16 +0100

    First Commit

---
+++ ProjectFiles/NetSolution/ApplicationNameLogic.cs
@@ -0,0 +1,24 @@
+#region Using directives
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.SerialPort;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CommunicationDriver;
+using FTOptix.Store;
+using FTOptix.SQLiteStore;
+using FTOptix.DataLogger;
+#endregion
+
+public class ApplicationNameLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        Label label = Owner as Label;
+        label.Text = Project.Current.BrowseName;
+    }
+
+    public override void Stop()
+    {
+    }
+}
